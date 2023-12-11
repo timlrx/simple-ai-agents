c5cf434d08c4eebb612a34fff611faa66c6525bb
Timothy Lin <timothy0336@hotmail.com>
2023-12-11 15:05:12 +0800

    feat: add cli shortcuts

--- simple_ai_agents/cli.py
+++ simple_ai_agents/cli.py
@@ -13,13 +13,14 @@ load_dotenv()
 @click.option("--character", default=None, help="Specify the character")
 @click.option("--prime/--no-prime", default=False, help="Enable priming")
 @click.option(
+    "-m",
     "--model",
     default="gpt-3.5-turbo",
     help="""Specify the LLM model e.g. gpt-3.5-turbo, ollama/mistral.
     Uses gpt-3.5-turbo by default.""",
 )
 @click.option("--temperature", default=0.7, help="LLM temperature. Default is 0.7.")
-@click.option("--system", default=SYSTEM_PROMPT, help="System prompt")
+@click.option("-s", "--system", default=SYSTEM_PROMPT, help="System prompt")
 @click.argument("prompt", required=False)
 def interactive_chat(character, prime, model, temperature, system, prompt):
     def read_prompt():
